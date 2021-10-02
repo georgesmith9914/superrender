55aff66367890c2eee8504b4e27f44e705535a8d
Chain Future <chain.future.forward@gmail.com>
2021-10-02 15:24:10 +0200

    Updated script for absolute iexec_in path

--- script.sh
+++ script.sh
@@ -11,7 +11,8 @@ echo "ls /iexec_in"
 
 export PATH="$PATH:/usr/local/blender"
 
-cmd="blender -b $IEXEC_IN/$IEXEC_INPUT_FILE_NAME_1  -o /iexec_out/out -f 1"
+#cmd="blender -b $IEXEC_IN/$IEXEC_INPUT_FILE_NAME_1  -o /iexec_out/out -f 1"
+cmd="blender -b /iexec_in/$IEXEC_INPUT_FILE_NAME_1  -o /iexec_out/out -f 1"
 $cmd
 
 echo "ls /iexec_out"
