59e4b0375f524619b7a506971a5f471ce2340454
Chain Future <chain.future.forward@gmail.com>
2021-11-14 14:58:25 +0100

    Updated files

--- script.sh
+++ script.sh
@@ -12,7 +12,7 @@ echo "ls /iexec_in"
 export PATH="$PATH:/usr/local/blender"
 
 #cmd="blender -b $IEXEC_IN/$IEXEC_INPUT_FILE_NAME_1  -o /iexec_out/out -f 1"
-cmd="blender -b /iexec_in/$IEXEC_INPUT_FILE_NAME_1  -o /iexec_out/out -f 1"
+cmd="blender -b /iexec_in/$IEXEC_INPUT_FILE_NAME_1  -o /iexec_out/out -s 1 -e 1"
 $cmd
 
 echo "ls /iexec_out"
